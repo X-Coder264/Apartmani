4b0c05bdc4ea3566ff9ea9fb8954bdd4eb208e59
Antonio <antonio.pauletich95@gmail.com>
2017-04-20 18:33:14 +0200

    fixed undefined index

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password'], ['rounds' => 15]),
-            'role_id' => $data['role_id'] ?: 2,
+            'role_id' => isset($data['role_id']) ? $data['role_id'] : 2,
         ]);
     }
 }
