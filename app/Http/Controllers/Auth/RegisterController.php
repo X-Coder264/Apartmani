41da30114ff1cd604f80789ddd0ab0e9702bd4b5
Antonio <antonio.pauletich95@gmail.com>
2017-04-20 19:01:59 +0200

    don't use create method to avoid mass assignment exception

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,11 +63,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password'], ['rounds' => 15]),
-            'role_id' => isset($data['role_id']) ? $data['role_id'] : 2,
-        ]);
+        $user = new User();
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->password = Hash::make($data['password'], ['rounds' => 15]);
+        $user->role_id = isset($data['role_id']) ? $data['role_id'] : 2;
+        $user->save();
+        return $user;
     }
 }
