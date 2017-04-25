c7ba41058d7dffd8b7ad1c96e311983b46b669a5
Antonio <antonio.pauletich95@gmail.com>
2017-04-25 07:14:02 +0200

    typo

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -17,7 +17,7 @@ class ProfileController extends Controller
     }
 
     /**
-     * Show the apartment edit form.
+     * Show the user edit form.
      *
      * @param User $user
      * @return \Illuminate\Http\Response
